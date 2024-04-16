7bf59433cbbed3c1ebf8ce12cae1438072667303
Gowtham Rao <rao@ohdsi.org>
2024-04-16 12:26:14 -0400

    Save cohort definition set from public atlas

--- extras/ScriptToCopyCohortDefinitionSet.R
+++ extras/ScriptToCopyCohortDefinitionSet.R
@@ -29,11 +29,18 @@ cohortDefinitionSet <-
   dplyr::rename(publicAtlasCohortId = cohortId) |>
   dplyr::mutate(cohortId = dplyr::row_number())
 
-saveRDS(object = cohortDefinitionSet, file = "CohortDefinitionSet/CohortDefinitionSet.RDS")
-cohortDefinitionSet <- readRDS("CohortDefinitionSet/CohortDefinitionSet.RDS")
+# save as RDS object
+saveRDS(object = cohortDefinitionSet, file = "CohortDefinitionSet/CohortDefinitionSetFromPublicAtlas.RDS")
+cohortDefinitionSet <- readRDS("CohortDefinitionSet/CohortDefinitionSetFromPublicAtlas.RDS")
 
+
+# save the full specification as csv files
 CohortGenerator::saveCohortDefinitionSet(
-  cohortDefinitionSet = cohortDefinitionSet,
+  cohortDefinitionSet = cohortDefinitionSet |> 
+    dplyr::select(cohortId,
+                  cohortName,
+                  json,
+                  sql),
   settingsFileName = "CohortDefinitionSet/inst/Cohorts.csv",
   jsonFolder = "CohortDefinitionSet/inst/cohorts",
   sqlFolder = "CohortDefinitionSet/inst/sql/sql_server"
@@ -81,3 +88,99 @@ cohortDefinitionSet <- CohortGenerator::getCohortDefinitionSet(
 )
 saveRDS(object = cohortDefinitionSet, file = "CohortDefinitionSet/CohortDefinitionSet.RDS")
 cohortDefinitionSet <- readRDS("CohortDefinitionSet/CohortDefinitionSet.RDS")
+
+
+# create cohort diagnostics study package
+outputFolder <- "CohortDiagnostics"  # location where you study package will be created
+
+
+########## Please populate the information below #####################
+version <- "v0.1.0"
+name <- "Myositis - cohort diagnostics"
+packageName <- "myositisCohortDiagnostics"
+skeletonVersion <- "v0.0.1"
+createdBy <- "bmarti86@jh.edu"
+createdDate <- Sys.Date() # default
+modifiedBy <- "bmarti86@jh.edu"
+modifiedDate <- Sys.Date()
+skeletonType <- "CohortDiagnosticsStudy"
+organizationName <- "OHDSI"
+description <- "Cohort diagnostics on Myositis."
+
+# compile them into a data table
+cohortDefinitionsArray <- list()
+for (i in (1:nrow(cohortDefinitionSet))) {
+  cohortDefinition <-
+    cohortDefinitionSet[i,]$json |> RJSONIO::fromJSON(digits = 23)
+  cohortDefinitionsArray[[i]] <- list(
+    id = cohortDefinitionSet[i,]$cohortId,
+    name = cohortDefinitionSet[i,]$cohortName,
+    expression = cohortDefinition
+  )
+}
+
+tempFolder <- tempdir()
+unlink(x = tempFolder,
+       recursive = TRUE,
+       force = TRUE)
+dir.create(path = tempFolder,
+           showWarnings = FALSE,
+           recursive = TRUE)
+
+specifications <- list(
+  id = 1,
+  version = version,
+  name = name,
+  packageName = packageName,
+  skeletonVersion = skeletonVersion,
+  createdBy = createdBy,
+  createdDate = createdDate,
+  modifiedBy = modifiedBy,
+  modifiedDate = modifiedDate,
+  skeletonType = skeletonType,
+  organizationName = organizationName,
+  description = description,
+  cohortDefinitions = cohortDefinitionsArray
+)
+
+jsonFileName <-
+  paste0(file.path(tempFolder, "CohortDiagnosticsSpecs.json"))
+write(x = specifications |> RJSONIO::toJSON(pretty = TRUE, digits = 23),
+      file = jsonFileName)
+
+
+download.file(url = "https://github.com/OHDSI/SkeletonCohortDiagnosticsStudy/archive/refs/heads/main.zip",
+              destfile = file.path(tempFolder, 'skeleton.zip'))
+unzip(
+  zipfile =  file.path(tempFolder, 'skeleton.zip'),
+  overwrite = TRUE,
+  exdir = file.path(tempFolder, "skeleton")
+)
+fileList <-
+  list.files(
+    path = file.path(tempFolder, "skeleton"),
+    full.names = TRUE,
+    recursive = TRUE,
+    all.files = TRUE
+  )
+DatabaseConnector::createZipFile(
+  zipFile = file.path(tempFolder, 'skeleton.zip'),
+  files = fileList,
+  rootFolder = list.dirs(file.path(tempFolder, 'skeleton'), recursive = FALSE)
+)
+
+hydraSpecificationFromFile <-
+  Hydra::loadSpecifications(fileName = jsonFileName)
+unlink(x = outputFolder, recursive = TRUE)
+dir.create(path = outputFolder,
+           showWarnings = FALSE,
+           recursive = TRUE)
+
+Hydra::hydrate(
+  specifications = hydraSpecificationFromFile,
+  outputFolder = outputFolder,
+  skeletonFileName = file.path(tempFolder, 'skeleton.zip')
+)
+unlink(x = tempFolder,
+       recursive = TRUE,
+       force = TRUE)
